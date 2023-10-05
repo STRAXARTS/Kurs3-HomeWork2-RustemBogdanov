4dc1e8b94fa049a4c5fdb0bd5d431acd51be2df4
Andrei Strakhov <straxarts@outlook.com>
2023-10-05 16:58:43 +0300

    выполнил задачу 10

---
+++ Exersize10/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите трехзначное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n < 100 | n > 999)
+    Console.WriteLine("Вы ввели не трёхзначное число. Запустите программу снова и повторите ввод.");
+else
+{
+    int ost1 = n % 10;
+    int ost2 = n % 100;
+    Console.WriteLine((ost2 - ost1) / 10);
+}
